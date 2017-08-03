2fb798ea06588278c45d12aaa2f94d6ee9ecf45a
David Watson <dave.watson8001@gmail.com>
2017-08-03 19:50:19 +0100

    Layout created

---
+++ DalesWindowWashers_app/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+import views
+
+urlpatterns = [
+    url(r'^DalesWindowWashers_app/$', views.homepage),
+    url(r'^/$', views.),
+    url(r'^/$', views.),
+    url(r'^/$', views.),
+]
