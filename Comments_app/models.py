d00f995f88d49b426466e2ec34e6409c5e40a35b
David Watson <dave.watson8001@gmail.com>
2017-08-08 02:55:35 +0100

    comments page

--- Comments_app/models.py
+++ Comments_app/models.py
@@ -8,10 +8,9 @@ from django.utils import timezone
 class Post(models.Model):
 
     author = models.ForeignKey('auth.User')
-    title = models.CharField(max_length=200)
+    type = models.CharField(max_length=20, blank=True, null=True)
     content = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
-    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.publish_date = timezone.now()
