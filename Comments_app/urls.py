64db38f4774fd1e57921dd716f514c40b8f64042
David Watson <dave.watson8001@gmail.com>
2017-08-08 17:33:55 +0100

    comments post form

--- Comments_app/urls.py
+++ Comments_app/urls.py
@@ -3,4 +3,6 @@ import views
 
 urlpatterns = [
     url(r'^comments/$', views.post_list),
+    url(r'^comments/(?P<id>\s+)/$', views.post_detail),
+    url(r'^post/new/$', views.new_post, name='new_post'),
 ]
--- Comments_app/views.py
+++ Comments_app/views.py
@@ -4,9 +4,15 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from .forms import CommentPostForm
 
 
 def post_list(request):
 
     posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, "Comments.html", {'posts': posts})
+
+
+def new_post(request):
+    form = CommentPostForm()
+    return render(request, 'commentspostform.html', {'form': form})
