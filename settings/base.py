f42b047de6b03ba0fb07c64dbc769850464750f3
David Watson <dave.watson8001@gmail.com>
2017-08-16 21:11:38 +0100

    static files

--- settings/base.py
+++ settings/base.py
@@ -88,7 +88,7 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-STATIC_URL = '/static/'
+STATIC_URL = 'https://morning-escarpment-70084.herokuapp.com/static/'
 STATIC_ROOT = 'staticfiles'
 
 STATICFILES_DIRS = (
