892e87c046fff4ca7b5bda5acfd4e96fae1b7dd8
David Watson <dave.watson8001@gmail.com>
2017-08-16 20:40:54 +0100

    static files

--- DalesWindowWashers/settings.py
+++ DalesWindowWashers/settings.py
@@ -1,38 +1,39 @@
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# SECURITY WARNING: Enter your own SECRET_KEY here
 SECRET_KEY = 'y&)wtg2ydn&rnu^3d^$no)d!wbzs3)31mo1f*c%@fk97im5=%s'
 
-ALLOWED_HOSTS = []
-SITE_ID = 2
+DEBUG = True
 
-# Application definition
+ALLOWED_HOSTS = []
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+
+STATICFILES_DIRS = (
+
+    os.path.join(BASE_DIR, 'static'),
+)
+
 INSTALLED_APPS = [
     'django.contrib.admin',
-    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.auth',
     'django.contrib.sites',
-    'django.contrib.flatpages',
-    'django_forms_bootstrap',
-    'paypal.standard.ipn',
-    'tinymce',
-    'emoticons',
-    'disqus',
     'Comments_app',
+    'django_forms_bootstrap',
     'Accounts_app',
+    'paypal.standard.ipn',
     'Products_app',
+    'emoticons',
+    'tinymce',
+    'disqus',
 ]
-
-AUTH_USER_MODEL = 'Accounts_app.User'
-AUTHENTICATION_BACKENDS = ('django.contrib.auth.backends.ModelBackend',
-                           'Accounts_app.backends.EmailAuth',)
-LOGIN_URL = '/login/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -40,21 +41,22 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
+
 ROOT_URLCONF = 'DalesWindowWashers.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
+
         'APP_DIRS': True,
+
         'OPTIONS': {
+
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
@@ -67,33 +69,59 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'DalesWindowWashers.wsgi.application'
 
+DATABASES = {
+
+    'default': {
+
+        'ENGINE': 'django.db.backends.sqlite3',
+
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
+
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
+
     {
+
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
+
     {
+
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_L10N = True
-USE_TZ = True
+AUTHENTICATION_BACKENDS = (
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+    'django.contrib.auth.backends.ModelBackend',
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    'Accounts_app.backends.EmailAuth',
 )
 
-TINYMCE_JS_ROOT = os.path.join(BASE_DIR, "static", "js",
-                               "tinymce", "tinymce.min.js")
+SITE_URL = 'https://morning-escarpment-70084.herokuapp.com/'
+
+PAYPAL_NOTIFY_URL = 'https://morning-escarpment-70084.herokuapp.com//a-very-hard-to-guess-url/'
+
+PAYPAL_RECEIVER_EMAIL = '<dynamitedave1-facilitator_api1.hotmail.co.uk>'
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+AUTH_USER_MODEL = 'Accounts_app.User'
+
+TINYMCE_JS_ROOT = os.path.join(BASE_DIR, "static", 'js', 'tinymce', 'tinymce.min.js')
