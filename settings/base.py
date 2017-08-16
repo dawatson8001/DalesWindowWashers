cf14ef415bb6af166d7c8a9e420ca6d7bfb371ab
David Watson <dave.watson8001@gmail.com>
2017-08-16 14:00:12 +0100

    static files

--- settings/base.py
+++ settings/base.py
@@ -93,7 +93,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = ''
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "/static/"),
 )
 
 TINYMCE_JS_ROOT = os.path.join(BASE_DIR, "static", "js",
