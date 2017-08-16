71d0a2e6fc09c6641516bc2f43a91332d1299618
David Watson <dave.watson8001@gmail.com>
2017-08-16 10:30:16 +0100

    deployment settings

--- settings/staging.py
+++ settings/staging.py
@@ -3,9 +3,10 @@ import dj_database_url
 DEBUG = False
 
 DATABASES = {
-    'default':
+    'default':{
+
         dj_database_url.config('mysql://b1176416fdcf8d:238a5581@us-cdbr-iron-east-05.cleardb.net/heroku_c29de6da34c32c7?reconnect=true')
-}
+}}
 
 # Paypal environment variables
 SITE_URL = '/morning-escarpment-70084.herokuapp.com/'
