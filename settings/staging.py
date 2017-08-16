2aaebbe4a73133ae648928ca92814d9eb1605330
David Watson <dave.watson8001@gmail.com>
2017-08-16 09:50:43 +0100

    deployment settings

---
+++ settings/staging.py
@@ -0,0 +1,12 @@
+from base import *
+import dj_database_url
+DEBUG = False
+
+DATABASES = {
+    'default': dj_database_url.config('mysql://b1176416fdcf8d:238a5581@us-cdbr-iron-east-05.cleardb.net/heroku_c29de6da34c32c7?reconnect=true')
+}
+
+# Paypal environment variables
+SITE_URL = '/morning-escarpment-70084.herokuapp.com/'
+PAYPAL_NOTIFY_URL = '/morning-escarpment-70084.herokuapp.com//a-very-hard-to-guess-url/'
+PAYPAL_RECEIVER_EMAIL = '<dynamitedave1-facilitator_api1.hotmail.co.uk>'
