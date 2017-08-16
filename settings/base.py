84e2a4acfb92c93df2d8352551631155d31d25bd
David Watson <dave.watson8001@gmail.com>
2017-08-16 22:18:33 +0100

    static files

--- settings/base.py
+++ settings/base.py
@@ -88,10 +88,12 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-STATIC_URL = 'static'
+STATIC_URL = '../static/'
 STATIC_ROOT = 'static'
 
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "../static"),
+)
 
 TINYMCE_JS_ROOT = os.path.join(BASE_DIR, "DalesWindowWashers/../static", "js",
                                "tinymce", "tinymce.min.js")
