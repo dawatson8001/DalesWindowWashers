15856b77b8535bff6e12d7ed6d60103a6792f904
David Watson <dave.watson8001@gmail.com>
2017-08-16 09:42:43 +0100

    deployment settings

--- settings/staging.py
+++ settings/staging.py
@@ -1,12 +1,9 @@
 from base import *
-
+import dj_database_url
 DEBUG = False
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config('mysql://b1176416fdcf8d:238a5581@us-cdbr-iron-east-05.cleardb.net/heroku_c29de6da34c32c7?reconnect=true')
 }
 
 # Paypal environment variables
