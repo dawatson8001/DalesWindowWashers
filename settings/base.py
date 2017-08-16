cfc9d55915a1fb91abf45357323be8365c590da5
David Watson <dave.watson8001@gmail.com>
2017-08-16 21:17:43 +0100

    static files

--- settings/base.py
+++ settings/base.py
@@ -88,9 +88,7 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-STATIC_URL = (
-    os.path.join(BASE_DIR, "static"),
-)
+STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
 
 STATICFILES_DIRS = (
