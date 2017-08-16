d1cadb5c218aaea57440bd6be5a68179ba3a627d
David Watson <dave.watson8001@gmail.com>
2017-08-17 00:18:24 +0100

    update clock

--- static/js/timer.js
+++ static/js/timer.js
@@ -1,14 +1,5 @@
-function myTimer(){
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('clock').innerHTML = h + ":" + m + ":" + s;
-    var t = setTimeout(startTime, 500);
-}
-function checkTime(i){
-    if (i<10){i = "0" + i}
-    return i;
+var myVar = setInterval(myTimer, 1000);
+function myTimer() {
+    var theTime = new Date();
+    document.getElementById('clock').innerHTML = theTime.toLocaleTimeString();
 }
