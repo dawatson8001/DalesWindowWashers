1ef2c4a33845d4e9619a67f5e17ecab84ad96be7
David Watson <dave.watson8001@gmail.com>
2017-08-16 11:52:39 +0100

    deployment settings

--- settings/staging.py
+++ settings/staging.py
@@ -7,7 +7,7 @@ DATABASES = {
 }
 
 # Paypal environment variables
-SITE_URL = 'morning-escarpment-70084.herokuapp.com'
+SITE_URL = '/morning-escarpment-70084.herokuapp.com/'
 PAYPAL_NOTIFY_URL = 'morning-escarpment-70084.herokuapp.com//a-very-hard-to-guess-url'
 PAYPAL_RECEIVER_EMAIL = '<dynamitedave1-facilitator_api1.hotmail.co.uk>'
 
