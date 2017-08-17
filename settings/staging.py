7d1ec41b7ba5c39af673e01d815cd555dca1cfd4
David Watson <dave.watson8001@gmail.com>
2017-08-17 22:41:55 +0100

    database files

--- settings/staging.py
+++ settings/staging.py
@@ -13,12 +13,12 @@ DEBUG = False
 #        'PORT': '3306',
 #    }}
 DATABASES = {
-    'default': dj_database_url.config('postgres://czsxgzxsffmciw:ab8c6f2f493abf6653b4aed94a9b1194c80b0112abb27c1e7f7640259983ccdc@ec2-107-22-211-182.compute-1.amazonaws.com:5432/d4e4ed0j43jfm5')
+    'default': dj_database_url.config('czsxgzxsffmciw:ab8c6f2f493abf6653b4aed94a9b1194c80b0112abb27c1e7f7640259983ccdc@ec2-107-22-211-182.compute-1.amazonaws.com:5432/d4e4ed0j43jfm5')
 }
 
 
 #DATABASES = {}
-#DATABASES['default'] = dj_database_url.config('mysql://b1176416fdcf8d:238a5581@us-cdbr-iron-east-05.cleardb.net/heroku_c29de6da34c32c7')
+#DATABASES['default'] = dj_database_url.config('postgres://czsxgzxsffmciw:ab8c6f2f493abf6653b4aed94a9b1194c80b0112abb27c1e7f7640259983ccdc@ec2-107-22-211-182.compute-1.amazonaws.com:5432/d4e4ed0j43jfm5')
 
 # Paypal environment variables
 SITE_URL = '/morning-escarpment-70084.herokuapp.com/'
