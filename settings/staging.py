7076480d5fc0af282da6982d7c5fce19a0395f8c
David Watson <dave.watson8001@gmail.com>
2017-08-17 23:24:21 +0100

    database files

--- settings/staging.py
+++ settings/staging.py
@@ -3,26 +3,8 @@ import dj_database_url
 
 DEBUG = False
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'NAME':   'heroku_c29de6da34c32c7',
-#        'USER': 'b1176416fdcf8d',
-#        'PASSWORD': '238a5581',
-#        'HOST': 'us-cdbr-iron-east-05.cleardb.net',
-#        'PORT': '3306',
-#    }}
-
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config()
-
-#DATABASES = {
-#    'default': dj_database_url.config('czsxgzxsffmciw:ab8c6f2f493abf6653b4aed94a9b1194c80b0112abb27c1e7f7640259983ccdc@ec2-107-22-211-182.compute-1.amazonaws.com:5432/d4e4ed0j43jfm5')
-#}
-
-
-#DATABASES = {}
-#DATABASES['default'] = dj_database_url.config('postgres://czsxgzxsffmciw:ab8c6f2f493abf6653b4aed94a9b1194c80b0112abb27c1e7f7640259983ccdc@ec2-107-22-211-182.compute-1.amazonaws.com:5432/d4e4ed0j43jfm5')
 
 # Paypal environment variables
 SITE_URL = '/morning-escarpment-70084.herokuapp.com/'
