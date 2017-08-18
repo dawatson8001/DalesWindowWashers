ce0e02da8a44d4d7c2470f37bbb4413fb3a07919
David Watson <dave.watson8001@gmail.com>
2017-08-18 02:18:27 +0100

    accounts form updated

--- Accounts_app/models.py
+++ Accounts_app/models.py
@@ -10,20 +10,19 @@ from django.db import models
 class AccountUserManager(UserManager):
     def _create_user(self, username, email, password, is_staff, is_superuser, first_name, last_name, **extra_fields):
         now = timezone.now()
-        if not User.objects.filter(email=self.cleaned_data['email']).exists():
-            if not email:
-                raise ValueError('The given username must be set')
+        if not email:
+            raise ValueError('The given username must be set')
 
-            email = self.normalize_email(email)
-            user = self.model(username=email, email=email, is_staff=is_staff,
+        email = self.normalize_email(email)
+        user = self.model(username=email, email=email, is_staff=is_staff,
                            is_active=True, is_superuser=is_superuser,
                            date_joined=now, first_name=first_name, last_name=last_name,
                            **extra_fields)
 
-            user.set_password(password)
-            user.save(using=self.__db)
+        user.set_password(password)
+        user.save(using=self.__db)
 
-            return user
+        return user
 
 
 class User(AbstractUser):
