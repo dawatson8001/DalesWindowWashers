1a5e344b0c5466cfcfede4cea570223045976ab1
David Watson <dave.watson8001@gmail.com>
2017-08-18 01:44:02 +0100

    accounts form updated

--- Comments_app/models.py
+++ Comments_app/models.py
@@ -4,14 +4,15 @@ from __future__ import unicode_literals
 from django.db import models
 from tinymce.models import HTMLField
 from django.contrib.auth.models import User
-from DalesWindowWashers import settings
+from settings import base
+
 
 class Post(models.Model):
 
     class Meta:
         app_label = "Comments_app"
 
-    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    user = models.ForeignKey(base.AUTH_USER_MODEL)
     content = HTMLField(blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
 
