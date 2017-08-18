660a1c4f812205dd7dc0516cd98076969e4ea894
David Watson <dave.watson8001@gmail.com>
2017-08-18 01:03:48 +0100

    comments link to user

--- Comments_app/models.py
+++ Comments_app/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from tinymce.models import HTMLField
-from Accounts_app.models import User
+from settings import base
 
 
 class Post(models.Model):
@@ -11,7 +11,7 @@ class Post(models.Model):
     class Meta:
         app_label = "Comments_app"
 
-    user = models.ForeignKey('Accounts_app.User')
+    user = models.ForeignKey(base.AUTH_USER_MODEL)
     content = HTMLField(blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
 
