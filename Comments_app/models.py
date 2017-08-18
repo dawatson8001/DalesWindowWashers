18126f22ed7c69caa1e2395ddd048e4828c86f99
David Watson <dave.watson8001@gmail.com>
2017-08-18 02:48:50 +0100

    accounts form updated

--- Comments_app/models.py
+++ Comments_app/models.py
@@ -11,7 +11,7 @@ class Post(models.Model):
     class Meta:
         app_label = "Comments_app"
 
-    user = models.ForeignKey(base.AUTH_USER_MODEL, related_name='posts')
+    user = 1
     content = HTMLField(blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
 
