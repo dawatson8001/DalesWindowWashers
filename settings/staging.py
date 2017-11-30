ff2b0a60da432e6c3ee5c76ce904516ff6bb2720
David Watson <dave.watson8001@gmail.com>
2017-11-30 17:17:11 +0000

    update hosting website

--- settings/staging.py
+++ settings/staging.py
@@ -7,6 +7,6 @@ DATABASES = {}
 DATABASES['default'] = dj_database_url.config()
 
 # Paypal environment variables
-SITE_URL = 'https://morning-escarpment-70084.herokuapp.com'
-PAYPAL_NOTIFY_URL = 'https://morning-escarpment-70084.herokuapp.com//a-very-hard-to-guess-url'
+SITE_URL = 'https://daleswindowwashers.herokuapp.com'
+PAYPAL_NOTIFY_URL = 'https://daleswindowwashers.herokuapp.com//a-very-hard-to-guess-url'
 PAYPAL_RECEIVER_EMAIL = 'dynamitedave1-facilitator@hotmail.co.uk'
